4595bcae5ff1c5875b432e07743263af7417152a
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2021-12-22 22:54:44 -0600

    created arrays for lowercase letters, uppercase letters, numbers, and special characters.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//create arrays for each character set 
+//create arrays for lowercase letters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+//create arrays for uppercase letters
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+//create arrays for numbers
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//create arrays for special characters
+var characters = ["!", "#", "$", "%", "&", "*", "+", "-", "/", "?", "@", "^", "_", "~"]
+
 
 function generatePassword() {
 
