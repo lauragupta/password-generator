fd238704eaa1b592acdb443718e3c2222a321095
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2021-12-25 21:43:42 -0600

    edits from proofreading text of js and readme.

--- script.js
+++ script.js
@@ -30,25 +30,25 @@ function generatePassword() {
 
   //alert if NAN or number is not in range
   if (isNaN(numberCharacters)  || numberCharacters < 8 || numberCharacters > 128) {
-    alert("You must choose a number from 8-128.");
+    alert("You must choose a number from 8-128. Please try again.");
     return;
   }
 
   //confirm to choose characters... Would you like to include uppercase letters
-  var useUppercase = confirm("You need to include at least one character set. Would you like to include uppercase letters? Cancel=no  OK=Yes");
+  var useUppercase = confirm("You need to include at least one character set. Would you like to include uppercase letters in your password? Cancel=No  OK=Yes");
 
   // confirm would you like to inlcude lower case letters
-  var useLowercase = confirm("You need to include at least one character set. Would you like to include lowercase letters? Cancel=no  OK=Yes");
+  var useLowercase = confirm("You need to include at least one character set. Would you like to include lowercase letters in your password? Cancel=No  OK=Yes");
 
   //confirm would you like to include numbers
-  var useNumbers = confirm("You need to include at least one character set. Would you like to include numbers? Cancel=no  OK=Yes");
+  var useNumbers = confirm("You need to include at least one character set. Would you like to include numbers in your password? Cancel=No  OK=Yes");
 
   //confirm would you like to include special characters
-  var useSpecialCharacters = confirm("You need to include at least one character set. Would you like to include Special Characters? Cancel=no  OK=Yes");
+  var useSpecialCharacters = confirm("You need to include at least one character set. Would you like to include Special Characters in your password? Cancel=No  OK=Yes");
 
   //alert if no choice was selected and repeat to step choose first character set
   if (!useUppercase && !useLowercase && !useNumbers && !useSpecialCharacters) { 
-    alert("You must choose at least one character set.");
+    alert("You must choose at least one character set. Please try again.");
     return;
   }
 
